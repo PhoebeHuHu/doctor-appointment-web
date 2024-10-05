d7ba87ff6879e6924d2c7f5c76a124824b1e31d4
peike-hu <phoebehu233@gmail.com>
2024-10-05 11:09:36 +1000

    frontend: login/signup page

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      {/* title */}
+      <div className='text-center text-2xl my-10 text-gray-500'>
+        <p>CONTACT <span className='text-gray-700'>US</span></p>
+      </div>
+
+      {/* contact */}
+      <div className='flex my-10 flex-col md:flex-row gap-12'>
+        <img src={assets.contact_image} alt="about" className='w-full md:max-w-[360px] h-auto block object-contain' />
+        <div className='flex flex-col justify-center gap-6 md:w-1/2 text-sm text-gray-500  items-start'>
+          <b className='text-lg text-gray-600'>OUR OFFICE</b>
+          <p>54709 Willms Station<br />Suite 350, Washington, USA</p>
+          <p>Tel: (415) 555‑0132<br />Email: greatstackdev@gmail.com</p>
+          <b className='text-lg text-gray-600'>CAREERS AT PRESCRIPTO</b>
+          <p>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 hover:bg-black hover:text-white transition-all duration-300'>Explore Jobs</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const Login = () => {
+  const [state, setState] = useState('Sign Up')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault()
+  }
+
+  // styles
+  const inputStyles = 'border border-x-zinc-300 rounded w-full p-2 mt-1'
+  const buttonStyles = 'bg-primary text-white w-full py-2 rounded-md text-base'
+  const linkStyles = 'text-primary underline cursor-pointer'
+  
   return (
-    <div>Login</div>
+    <form className='min-h-[80vh] flex items-center'>
+      <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg'>
+        <p className='text-lg font-semibold'>{state === 'Sign Up'?'Create Account':'Login'}</p>
+        <p>Please {state === 'Sign Up'?'sign up':'log in'} to book appointment</p>
+        <div className={`w-full ${state === 'Sign Up'? '':'hidden'}`}>
+          <p>Full Name</p>
+          <input className={inputStyles} type="text" onChange={(e)=>setName(e.target.name)} value={name} required/>
+        </div>
+        <div className='w-full'>
+          <p>Email</p>
+          <input className={inputStyles} type="email" onChange={(e)=>setName(e.target.email)} value={email} required/>
+        </div>
+        <div className='w-full'>
+          <p>Password</p>
+          <input className={inputStyles} type="password" onChange={(e)=>setName(e.target.password)} value={password} required/>
+        </div>
+        <button className={buttonStyles}>{state === 'Sign Up'?'Create Account':'Login'}</button>
+        {
+          state === 'Sign Up'
+          ?<p>Already have an account? <span className={linkStyles} onClick={()=>setState('Login')}>Login here</span></p>
+          :<p>Do not have an account? <span className={linkStyles} onClick={()=>setState('Sign Up')}>Create account</span></p>
+        }
+      </div>
+    </form>
   )
 }
 
